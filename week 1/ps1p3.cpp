e04b38f3095022703bd335059e46ad3a30615b8e
david-adeyemo <oluwatofunmiadeyemo@triton.edu>
2026-09-01 01:28:50 -0500

    Add files via upload

---
+++ week 1/ps1p3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string lastName;
+    double score;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    cout << "Enter your score: ";
+    cin >> score;
+
+    cout << lastName << " has a score of " << score << "." << endl;
+
+    return 0;
+} 
